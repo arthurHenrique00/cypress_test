154fbc94f158a292e34c2f27780168402182fe98
Arthur Henrique <arthurcostalima88@gmail.com>
2024-09-22 19:32:32 -0300

    Final

---
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Testes para a home', () => {
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+
+    it('Adicionar contato', () => {
+        cy.get('input[type="text"]').type('Arthur Henrique')
+        cy.get('input[type="email"]').type('arthur@gmail.com')
+        cy.get('input[type="tel"]').type('44 9999 9999')
+        cy.get('.adicionar').click()
+    })
+
+    it('Editar contato', () => {
+        cy.get('.edit').click()
+        cy.get('input[type="text"]').type('Arthur Henrique2')
+        cy.get('input[type="email"]').type('arthur2@gmail.com')
+        cy.get('input[type="tel"]').type('44 9999 8888')
+        cy.get('.alterar').click()
+    })
+
+    it('Remover contato', () => {
+        cy.get('.delete').click()
+    })
+})
